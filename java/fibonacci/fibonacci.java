22f0c3fb3085a09c13519fb0c917fdf85e6ece5c
Eduard le Roux <eduard.leroux@jumo.world>
2018-04-13 08:23:07 +0200

    Testing recursicve solution

--- java/fibonacci/fibonacci.java
+++ java/fibonacci/fibonacci.java
@@ -1,12 +1,21 @@
 class Fibonacci{
 
-    public Fibonacci() {
-        System.out.println("Test this");
+    public Fibonacci(int number) {
+
+    }
+
+    pubic static void fibonacci(int n, int result) {
+        if(number == 1 || number == 0) {
+            return result;
+        }
+
+        fibonacci(n-1, result) + fibonacci(n-2, result);
     }
 
 
     public static void main(String[] args) {
-        new Fibonacci();
+        int number = 5;
+        new Fibonacci(number);
 
     }
 }
