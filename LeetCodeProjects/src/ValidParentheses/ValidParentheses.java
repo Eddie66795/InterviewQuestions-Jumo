d5509fcd99383bb24509332fb33f7a4e7e80121f
Eduard le Roux <eduard.leroux@jumo.world>
2018-08-07 07:10:50 +0200

    ValidParentheses

--- LeetCodeProjects/src/ValidParentheses/ValidParentheses.java
+++ LeetCodeProjects/src/ValidParentheses/ValidParentheses.java
@@ -1,64 +1,119 @@
 package ValidParentheses;
 
+
 import java.util.Stack;
 
 public class ValidParentheses {
 
-    public ValidParenthesesInput[] getInput() {
+    public final Character OPEN_P = '(';
+    public final Character CLOSE_P = ')';
 
-        ValidParenthesesInput test[] = {
-                new ValidParenthesesInput("(())", true),
-                new ValidParenthesesInput("()()", true),
-        };
+    public final Character OPEN_B = '{';
+    public final Character CLOSE_B = '}';
 
-        return test;
-    }
+    public final Character OPEN_BR = '[';
+    public final Character CLOSE_BR = ']';
 
-    public void validate() {
-        ValidParenthesesInput getInput[] = getInput();
-        for(int i = 0; i < getInput.length; i++) {
-            String input = getInput[i].input;
-            if(isValid(getInput[i].input) != getInput[i].isValid) {
-                System.out.println("THERE WAS A PROBLEM");
-                System.out.println(input);
+    public boolean isValid(String s) {
+        if(s == null) {
+            return false;
+        } else if(s == "") {
+            return true;
+        } else if(s.length() % 2 != 0) {
+            return false;
+        } else if( isClosed(s.charAt(0))) {
+            return false;
+        }
+
+        Stack<Character> temp = new Stack<Character>();
+        for(int i = 0; i < s.length(); i++) {
+            Character inputChar = s.charAt(i);
+            if(isOpen(inputChar) || isClosed(inputChar)) {
+                if(isOpen(inputChar)) {
+                    temp.push(inputChar);
+                } else {
+                    Character popped = temp.pop();
+                    if (popped != getOpposite(inputChar)) {
+                        return false;
+                    }
+                }
+            } else {
+                return false;
             }
         }
-        System.out.println("TEST PASSED");
+        return temp.isEmpty();
     }
 
-    public final char open_parenthesis = '(';
-    public final char close_parenthesis =')';
-    public int parenthesis_count;
-
-
-    public boolean isValid(String input) {
-        parenthesis_count = 0;
-        char inputElements[] = input.toCharArray();
-
-        for(int i = 0; i < inputElements.length; i++) {
-            char temp = inputElements[i];
-            if(temp == open_parenthesis) {
-                parenthesis_count += 1;
-            } else if(temp == close_parenthesis) {
-                parenthesis_count -= 1;
-            }
+    public Character getOpposite(Character inputChar) {
+        if(inputChar == CLOSE_P) {
+            return OPEN_P;
+        } else if(inputChar == CLOSE_B) {
+            return OPEN_B;
+        } else {
+            return OPEN_BR;
         }
-        return parenthesis_count == 0;
     }
 
+    public boolean isOpen(Character input) {
+        if(input == OPEN_B || input == OPEN_BR || input == OPEN_P) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isClosed(Character input) {
+        if(input == CLOSE_B || input == CLOSE_BR || input == CLOSE_P) {
+            return true;
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
-        new ValidParentheses().validate();
+        new ValidParentheses();
     }
 
-    class ValidParenthesesInput {
+    public ValidParentheses() {
+        for(InputParams i: generateInput()) {
+            boolean expecteed_result  = i.isValid;
+            boolean actual_result = isValid(i.inputParam);
 
-        public String input;
-        public boolean isValid;
+            if(actual_result != expecteed_result) {
+                System.out.println("!!!!PROBLEM!!!!");
+                System.out.println("i.inputParam");
+                System.out.println();
+            } else {
+                System.out.println("TRUE");
+            }
+        }
 
-        public ValidParenthesesInput(String input, boolean isValid) {
-            this.input = input;
-            this.isValid = isValid;
+        System.out.println("DONE");
+    }
+
+    public InputParams[] generateInput() {
+        InputParams[] val = {
+                new InputParams("", true),
+//                new InputParams("()()", true),
+//                new InputParams("(())", true),
+//                new InputParams("(()", false),
+//                new InputParams("({})[(())]", true),
+//                new InputParams("{", false),
+//                new InputParams("} ", false),
+//                new InputParams("){ ", false),
+        };
+        return val;
+    }
+
+
+    public class InputParams {
+
+        private String inputParam;
+        private boolean isValid;
+
+        public InputParams(String inputParam, boolean isValid) {
+            if(inputParam.length() != 0) {
+                this.inputParam = inputParam;
+                this.isValid = isValid;
+            }
         }
     }
 }
